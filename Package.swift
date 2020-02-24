be316c5e698b7b3f41f6889f4b1943f83ca0c9ad
Javier Segura <javier@miolabs.com>
2020-02-24 13:04:01 +0100

    Added connection for MYSQL C API

--- Package.swift
+++ Package.swift
@@ -19,9 +19,17 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .systemLibrary(
+            name: "CMySQL",
+            pkgConfig: "mysqlclient",
+            providers: [
+                .brew(["mysql"]),
+                .apt(["libmysqlclient-dev"])
+            ]
+        ),
         .target(
             name: "MIODBMySQL",
-            dependencies: ["MIODB"]),
+            dependencies: ["MIODB", "CMySQL"]),
         .testTarget(
             name: "MIODBMySQLTests",
             dependencies: ["MIODBMySQL"]),
--- Sources/MIODBMySQL/MIODBMySQL.swift
+++ Sources/MIODBMySQL/MIODBMySQL.swift
@@ -1,4 +1,69 @@
 
-open class MIODBMySQL: MIODB {
+import Foundation
+import MIODB
+import CMySQL
 
+open class MIODBMySQL: MIODB {
+    
+    let defaultPort:Int32 = 5432
+    let defaultUser = "root"
+    let defaultDatabase = "public"
+    
+    var dbconnection:UnsafeMutablePointer<MYSQL>?
+        
+    open override func connect(){
+        if port == nil { port = defaultPort }
+        if user == nil { user = defaultUser }
+        if database == nil { database = defaultDatabase }
+        
+        let conn = mysql_init(nil)
+        
+        if mysql_real_connect(dbconnection, host!, user!, password!, database!, 0, nil, 0) != nil {
+            dbconnection = conn
+        }
+    }
+    
+    open override func connect(scheme:String?){
+        connect()
+        changeScheme(scheme)
+    }
+    
+    open override func disconnect() {
+        mysql_close(dbconnection)
+        dbconnection = nil
+    }
+    
+    open override func executeQueryString(_ query:String) throws -> [Any]{
+        
+//        let status = mysql_query(dbconnection, query.cString(using: .utf8))
+//        if status == nil {
+//            return []
+//        }
+
+//        res = mysql_store_result(dbconnection); // Get the Result Set
+//
+//        if res == nil {
+//            return []
+//        }
+        
+        var items:[Any] = []
+        
+/*
+            for row in 0..<PQntuples(res) {
+                var item = [String:Any]()
+                for col in 0..<PQnfields(res){
+                    if PQgetisnull(res, row, col) == 1 {continue}
+                    
+                    let colname = String(cString: PQfname(res, col))
+                    let type = PQftype(res, col)
+                    let value = PQgetvalue(res, row, col)
+                    
+                    item[colname] = convert(value: value!, withType: type)
+                }
+                items.append(item)
+            }
+           */
+                            
+        return items
+    }
 }
